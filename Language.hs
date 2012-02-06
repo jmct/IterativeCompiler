b87b847b8076d10f7ba33a1cabea3b40cfe8d336
Jose Calderon <jose@jmct.cc>
2012-02-06 11:50:26 +0000

    Added comments to language type

--- Language.hs
+++ Language.hs
@@ -7,16 +7,17 @@ type IsRec = Bool
 type Alter a = (Int, [a], Expr a)
 
 data Expr a
-    = EVar Name
-    | ENum Int
-    | EConstr Int Int
-    | EAp (Expr a) (Expr a)
-    | ELet
-        IsRec
-        [(a, Expr a)]
-        (Expr a)
-    | ECase
-        (Expr a)
-        [Alter a]
-    | ELam [a] (Expr a)
+    = EVar Name             --Variables (their name)
+    | ENum Int              --Numbers
+    | EConstr Int Int       --Constructors (the reference tag and the arity)
+    | EAp (Expr a) (Expr a) --Application of expression
+    | ELet                  --Let declaration
+        IsRec               --Boolean (True == Recursive Let)
+        [(a, Expr a)]       --list of definitions
+        (Expr a)            --body of let
+    | ECase                 --Case declaration
+        (Expr a)            --expression to compare
+        [Alter a]           --list of alternatives to execute
+    | ELam [a] (Expr a)     --Lambda expression
     deriving Show
+
