bff8e24f45fca326b43394db4479a50a213544fe
Jose Calderon <jose@jmct.cc>
2012-02-27 20:57:16 +0000

    Added line number for each token as part of clex

--- Parser.hs
+++ Parser.hs
@@ -3,28 +3,29 @@ module Parser where
 import Language
 import Char
 
-type Token = String
+type Token = (Int, String)
 --note that a token will never be empty!
 
 {-clex will take the source code (as a long string) and produce a set of tokens 
  - the whitespace is 'eaten up' first with a check to isSpace, then numbers are
  - are taken in their entirety and lastly we have variables. Variables must
  - start with a character but can then contain anything.-}
-clex :: String -> [Token]
-clex [] = []
-clex (c:cs)
-    | isWhiteSpace c = clex cs
-    | isDigit c = numToken : clex rest_digit
-    | isAlpha c = varToken : clex rest_alpha 
-    | isComment (c:cs) = clex rest_ignrCmnt
-    | isTwoCharOp (c:cs) = twoChrOpTkn : clex restTwoChar 
-    | otherwise  = [c] : clex cs
+clex :: Int -> String -> [Token]
+clex _ [] = []
+clex line (c:cs)
+    | c == '\n' = clex (line + 1) cs
+    | isWhiteSpace c = clex line cs
+    | isDigit c = (line, numToken) : clex line rest_digit
+    | isAlpha c = (line, varToken) : clex line rest_alpha 
+    | isComment (c:cs) = clex (line + 1) rest_ignrCmnt
+    | isTwoCharOp (c:cs) = (line, twoChrOpTkn) : clex line restTwoChar 
+    | otherwise  = (line, [c]) : clex line cs
         where
             numToken       = c : takeWhile isDigit cs
             rest_digit     = dropWhile isDigit cs
             varToken       = c : takeWhile isIdChar cs
             rest_alpha     = dropWhile isIdChar cs
-            rest_ignrCmnt  = dropWhile (/= '\n') cs
+            rest_ignrCmnt  = drop 1 $ dropWhile (/= '\n') cs --need to drop the \n too
             twoChrOpTkn    = take 2 (c:cs)
             restTwoChar    = drop 2 (c:cs)
 
@@ -40,7 +41,7 @@ isIdChar :: Char -> Bool
 isIdChar c = isAlpha c || isDigit c || (c == '_')
 
 isWhiteSpace :: Char -> Bool
-isWhiteSpace c = c `elem` " \t\n"
+isWhiteSpace c = c `elem` " \t\r"
 
 isComment :: String -> Bool
 isComment ('-':'-':rest) = True
