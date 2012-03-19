0442f70bced462fb6c374d77b18f7be19540fef9
Jose Calderon <jose@jmct.cc>
2012-03-19 17:37:34 +0000

    completed infix parsing and fixed bug in pApplication

--- Parser.hs
+++ Parser.hs
@@ -1,6 +1,7 @@
 module Parser where
 
 import Language
+import Data.List
 import Data.Char
 
 type Token = (Int, String)
@@ -43,6 +44,9 @@ keywords = ["let","letrec","case","in","of","Pack"]
 twoCharOps :: [String]
 twoCharOps = ["==","~=",">=","<=","->"]
 
+relOps :: [String]
+relOps = "<" : ">" : (delete "->" twoCharOps)
+
 isTwoCharOp :: String -> Bool
 isTwoCharOp str = (take 2 str) `elem` twoCharOps
 
@@ -59,6 +63,10 @@ isComment _ = False
 --We define a Parser as a function that takes a list of tokens and returns
 --a tuple of the parsed value and the remaining list of tokens.
 type Parser a = [Token] -> [(a, [Token])]
+
+--For parsing infix operators we need a type for partial expressions
+--NoOp is for when the parsing of an infix fails
+data PartialExpr = NoOp | FoundOp Name CoreExpr
 
 --For Literal values we will take a string and a Parser for type String and 
 --compare the provided string with the value of the fist token's String
@@ -184,6 +192,8 @@ pSc = pThen4 makeSc pVar (pZeroOrMore pVar) (pLiteral "=") pExpr
     where
         makeSc name args eq expr = (name, args, expr)        
 
+pFull f = pThen (\x y -> x) f (pEnd)
+
 --------------------------------------------------------------------------------
 --The parser functions below are for the grammar outlined in Figure 1.1 of IFL
 
@@ -200,34 +210,79 @@ pSc = pThen4 makeSc pVar (pZeroOrMore pVar) (pLiteral "=") pExpr
  - expr1c -> | expr1        --This means that expr1c is either an OR followed
  -         | []             --by an expr1 or an empty list. This, and all
  -                          --similar cases below require partial expressions
- -
+ -}
+pExpr1 :: Parser CoreExpr
+pExpr1 = pThen assembleOp pExpr2 pExpr1c
+
+pExpr1c :: Parser PartialExpr
+pExpr1c = (pThen FoundOp (pLiteral "|") pExpr1) `pAlt` (pEmpty NoOp)
+
+assembleOp :: CoreExpr -> PartialExpr -> CoreExpr
+assembleOp e1 NoOp = e1
+assembleOp e1 (FoundOp op e2) = EAp (EAp (EVar op) e1) e2
+{-
  - expr2  -> expr3 expr2c
  - expr2c -> & expr2
  -         | []
- -
- - expr3  -> expr4 expr3c
+ -}
+pExpr2 :: Parser CoreExpr
+pExpr2 = pThen assembleOp pExpr3 pExpr2c
+
+pExpr2c :: Parser PartialExpr
+pExpr2c = (pThen FoundOp (pLiteral "&") pExpr2) `pAlt` (pEmpty NoOp)
+
+{- expr3  -> expr4 expr3c
  - expr3c -> relop expr3
  -         | []
- -
- - expr4  -> expr5 expr4c
+ -}
+pExpr3 :: Parser CoreExpr
+pExpr3 = pThen assembleOp pExpr4 pExpr3c
+
+pExpr3c :: Parser PartialExpr
+pExpr3c = (pThen FoundOp pRelOp pExpr3) `pAlt` (pEmpty NoOp)
+
+pRelOp :: Parser String
+pRelOp = pSat (`elem` relOps)
+
+{- expr4  -> expr5 expr4c
  - expr4c -> + expr4
  -         | - expr5
  -         | []
- -
- - expr5  -> expr6 expr5c
+ -}
+pExpr4 :: Parser CoreExpr
+pExpr4 = pThen assembleOp pExpr5 pExpr4c
+
+pExpr4c :: Parser PartialExpr
+pExpr4c = (pThen FoundOp (pLiteral "+") pExpr4) `pAlt` 
+          (pThen FoundOp (pLiteral "-") pExpr5) `pAlt`
+          (pEmpty NoOp)
+
+{- expr5  -> expr6 expr5c
  - expr5c -> * expr5
- -         | / expr5
+ -         | / expr6
  -         | []
- -
- - expr6  -> aexpr1 ... aexprN   where N >= 1
+ -}
+pExpr5 :: Parser CoreExpr
+pExpr5 = pThen assembleOp pExpr6 pExpr5c
+
+pExpr5c :: Parser PartialExpr
+pExpr5c = (pThen FoundOp (pLiteral "*") pExpr5) `pAlt` 
+          (pThen FoundOp (pLiteral "/") pExpr6) `pAlt`
+          (pEmpty NoOp)
+
+{- expr6  -> aexpr1 ... aexprN   where N >= 1
  - -}
+pExpr6 :: Parser CoreExpr
+pExpr6 = pApplication
+
 {-The parsing for Let expressions and Recursive-Let expressions are defined
 --independently, this is because the core Language uses different keywords for
 --the two. -}
 {-PExpr will string together all of parsers for the valid expressions as defined in Language.hs
  -There will be one parser for each expression type and a few helper functions/parsers -}
 pExpr :: Parser CoreExpr
-pExpr = pLet `pAlt` pLetRec `pAlt` pCase `pAlt` pLambda `pAlt` pAexpr `pAlt` pApplication
+pExpr = pLet `pAlt` pLetRec `pAlt` pCase `pAlt` pLambda 
+        `pAlt` pExpr1
 
 pAexpr :: Parser CoreExpr
 pAexpr = pVarExpr `pAlt` pNumExpr `pAlt` pConstr `pAlt` pParen
@@ -310,7 +365,7 @@ pLambVars :: Parser [Name]
 pLambVars = pOneOrMore pVar
 
 pApplication :: Parser CoreExpr
-pApplication = pThen EAp ((pOneOrMore pAexpr) `pApply` mkApChain) pAexpr
+pApplication = ((pOneOrMore pAexpr) `pApply` mkApChain) 
 
 mkApChain :: [CoreExpr] -> CoreExpr
 mkApChain (x:[]) = x
