f6212207ca9faf3e6c47c750b7346fdc25d64e11
Jose Calderon <jose@jmct.cc>
2012-03-28 19:01:37 +0100

    Finished heap utility functions, need to add Association list functions

--- Heap.hs
+++ Heap.hs
@@ -8,13 +8,48 @@ type Addr = Int
 --list of objects (a tuple consisting of the address and the value).
 type Heap a = (Int, [Addr], [(Addr, a)])
 
+--To remove an item from the heap, we only really need to worry about how to 
+--remove something from the contents (The association list). The other parts of
+--the heap can be dealt with trivially.
+remove :: [(Int, a)] -> Int -> [(Int, a)]
+remove [] a = error ("Attempting to free or update a non-allocated address "
+                     ++ showAddr a)
+remove ((a',n) : contents) a
+    | a == a' = contents
+    | a /= a' = (a',n) : remove contents a
+
 --The function creating the initial heap. (We know that there isn't actually
 --infinite memory...)
 hInitial :: Heap a
 hInitial = (0, [1..] [])
 
 hAlloc :: Heap a -> a -> (Heap a, Addr)
-hAlloc (size, (next:free), contents) n = ((size+1, free, (next,n) : contents), next)
+hAlloc (size, (next:free), contents) n 
+                                = ((size+1, free, (next,n) : contents), next)
+
 
 hUpdate :: Heap a -> Addr -> a -> Heap a
+hUpdate (size, free, contents) a n = (size, free, (a,n) : remove contents a)
+
+
 hFree :: Heap a -> Addr -> Heap a
+hFree (size, free, contents) a = (size-1, a:free, remove contents a)
+
+hLookup :: Heap a -> Addr -> a
+hLookup (size, free, contents) addr
+    = aLookup contents a (error ("Can't find node " ++ showAddr addr 
+                                                    ++ " in heap"))
+
+hAddresses :: Heap a -> [Addr]
+hAddresses (_, _, contents) = [addr | (addr, node) <- contents]
+
+hSize :: Heap a -> Int
+hSize (size, _, _) = size
+
+hNull :: Int
+hNull = 0
+
+hIsNull a = a == 0
+
+showAddr :: Int -> String
+showAddr a = "#" ++ show a
