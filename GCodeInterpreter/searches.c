962c9a37d82cdcd05c150b653d0fd00a6b4aced1
Jose Manuel Calderon Trilla <jose@jmct.cc>
2014-04-07 14:28:13 +0100

    Random search working properly

--- GCodeInterpreter/searches.c
+++ GCodeInterpreter/searches.c
@@ -6,13 +6,36 @@
 #include "searches.h"
 
 void prettyPrSwitches(parSwitch* switches, int nSwitch) {
-    puts("Bitstring used: ");
+    fputs("Bitstring used: ", stdout);
     int i;
     for (i = 0; i < nSwitch; i++) {
         printf("%d", switches[i].pswitch);
     }
     puts("\n");
 }
+
+int * mkSArray(parSwitch* swtchs, int nSwitch) {
+    int * sArray = malloc(sizeof(int) * nSwitch); 
+    int i;
+    for (i = 0; i < nSwitch; i++) {
+        sArray[i] = swtchs[i].pswitch;
+    }
+
+    return sArray;
+}
+
+void swtchToArray(parSwitch* swtchs, int nSwitch, int* sArray) {
+    int i;
+    for (i = 0; i < nSwitch; i++) {
+        sArray[i] = swtchs[i].pswitch;
+    }
+}
+    
+
+typedef struct {
+    int *swtchs;
+    unsigned int rCount;
+} elite;
 
 /*
 int * makeIntArray(parSwitch *switches, 
@@ -33,16 +56,31 @@ parSwitch* iterate(parSwitch* switches, int nSwitch, StatTable* gStat, enum sear
         return switches;
     }
     else if (sType == RAND) {
+        elite best;
+        best.swtchs = mkSArray(switches, nSwitch);
         unsigned int gr;
         int i;
         for (i = 1; i <= maxI; i++) {
             gr = 0;
             gr = executeProg(switches, prog, nSwitch);
+            if (i == 1) {
+                best.rCount = gr;
+            }
+            else if (gr < best.rCount) {
+                swtchToArray(switches, nSwitch, best.swtchs);
+                best.rCount = gr;
+            }
+
             printf("Run %d took %u reductions\n", i, gr);
             prettyPrSwitches(switches, nSwitch);
             randMutate(switches, nSwitch);
         }
-
+        
+        fputs("Best performing configuration: ", stdout);
+        for (i = 0; i < nSwitch; i++) {
+            printf("%d", best.swtchs[i]);
+        }
+        printf(" taking %u reductions\n", best.rCount);
     }
 
     /* This shouldn't happen */
