92e211f9000e3a6237e63d026e2552726666ed9b
José Manuel Calderón Trilla <jmct+git@jmct.cc>
2015-02-26 20:20:00 +0000

    script v2

--- runEx.sh
+++ runEx.sh
@@ -1,6 +1,8 @@
-flags="-I100 -S -H100 -R100"
+flags="-I50 -S -H100 -R100"
 os="20 40 80 160 320"
 cs="1 2 4 8 16 24"
+os="5 10"
+cs="1 2"
 
 for flag in $flags; do
     for o in $os; do
@@ -8,7 +10,7 @@ for flag in $flags; do
             mkdir run-$flag-$o-$c
             cd run-$flag-$o-$c
             echo ln -s ../*.gcode
-            echo gun $flag -O$o -C$c $1 2\> errorLog.txt \> runLog.txt
+            echo gun $flag -O$o -C$c *.gcode 2\> errorLog.txt \> runLog.txt
             cd ..
             echo
         done
